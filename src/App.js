b3f643e1ed341a1c26acea4a01bd3e544a09eb25
kenichi miyata <miyataken999@bpmboxes.com>
2022-07-18 10:08:00 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn ssss React
           </a>
         </header>
       </div>
