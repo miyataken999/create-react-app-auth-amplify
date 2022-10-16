5be3f4fb5b85fac26ecc86802bd469ed2d792e0e
kenichi miyata <miyataken999@bpmboxes.com>
2022-10-16 14:01:53 +0900

    df
    fdaf

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn dddddddddddddddddd React
+            Learn ddddddddddffdfaddddddddd React
           </a>
         </header>
       </div>
